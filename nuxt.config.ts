4249f6aaba1498c1ce57d6552e98a594e8532631
delios <moynetloeiz@gmail.com>
2023-02-03 19:35:13 +0100

    Fixed Syntax Highlighting on all modes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     ],
     content: {
         highlight: {
-            theme: 'github-light'
+            theme: 'github-dark'
         }
     },
     colorMode: {
