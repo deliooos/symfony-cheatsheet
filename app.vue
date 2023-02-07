129276570474a7565884cfd1c2c3580ddb78997c
Loeiz Moynet <moynetloeiz@gmail.com>
2023-02-07 16:46:50 +0100

    Changed favicon to Symfony Logo

--- app.vue
+++ app.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+useHead({
+  title: 'SFCS',
+  meta: [
+    {
+      charset: "utf-8"
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    },
+    {
+      "http-equiv": "X-UA-Compatible",
+      content: "ie-edge"
+    },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    }
+  ]
+})
+</script>
+
 <template>
   <Header/>
   <NuxtPage/>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,4 @@
 <script setup lang="ts">
-import HomeCodex from "~/components/HomeCodex.vue";
-
-useHead({
-  title: 'SFCS',
-  meta: [
-    {
-      charset: "utf-8"
-    },
-    {
-      name: "viewport",
-      content: "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    },
-    {
-      "http-equiv": "X-UA-Compatible",
-      content: "ie-edge"
-    }
-  ]
-})
 </script>
 
 <template>
