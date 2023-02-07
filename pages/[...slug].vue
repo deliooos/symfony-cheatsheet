eeca55e02e78bda1d8458cf3439987a45aa5320d
Loeiz Moynet <moynetloeiz@gmail.com>
2023-02-07 14:42:36 +0100

    Fixed a few things

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,7 +5,7 @@
         <template #not-found>
           <main class="classic-main flex flex-col items-center">
             <nuxt-img src="/assets/404.svg" alt="Image de loupe illustrant l'erreur 404" />
-            <h1 class="mb-0 text-5xl font-bold text-center">Page introuvable</h1>
+            <h1 class="mb-0 text-5xl font-bold text-center">Page introuvable.</h1>
             <p class="m-0 mt-2">La page que vous cherchez est soit indisponible soit en cours de création.</p>
             <NuxtLink to="/" class="btn btn-primary mt-4">Retourner à l'accueil</NuxtLink>
           </main>
