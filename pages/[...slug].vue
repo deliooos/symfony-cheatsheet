9fbac12b3c6c1efb6af767de5fe7eef579df408c
Loeiz Moynet <moynetloeiz@gmail.com>
2023-02-08 13:49:03 +0100

    Added read time, edit button (wip), tags and content to article pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,23 +2,27 @@
   // @ts-ignore
   import readingTime from "reading-time/lib/reading-time";
 
-  // const route = useRoute()
-  // const text = await queryContent(route.fullPath).find()
-  // const stats = readingTime(text)
+  const route = useRoute()
+  const article = await queryContent(route.path).findOne()
+
+  let readTime = ref('')
+  let headers = ref<Array<HTMLHeadingElement>>([])
 
   onMounted(() => {
     const content = document.querySelector('.prose')
+    headers.value = Array.from(document.querySelectorAll('h2'))
 
     if (content) {
       const stats = readingTime(content.innerHTML)
-      const readTime = stats.minutes > 1 ? `${stats.minutes} minutes` : `${stats.time} secondes`
+      const time = Math.round(stats.minutes)
+      readTime.value = time > 1 ? `Environ ${time} minutes` : `Environ ${stats.time} secondes`
     }
   })
 </script>
 
 <template>
   <main class="classic-main flex gap-10">
-    <div class="prose max-w-none w-4/5">
+    <div class="prose max-w-none md:w-4/5">
       <ContentDoc>
         <template #not-found>
           <main class="classic-main flex flex-col items-center">
@@ -30,14 +34,25 @@
         </template>
       </ContentDoc>
     </div>
-    <div class="w-1/5">
-      <div class="sticky top-6">
-        <h5 class="font-semibold text-xl">À propos</h5>
-        <ul>
-          <li>{{ readTime }}</li>
-          <li></li>
-          <li></li>
+    <div class="hidden md:block w-1/5">
+      <div class="sticky top-6 flex flex-col gap-4">
+        <h5 class="-mb-1 font-semibold text-xl">À propos</h5>
+        <ul class="space-y-1">
+          <li class="inline-flex items-center gap-2"><Icon name="ph:clock" size="1.2rem"/> {{ readTime }}</li>
+          <li class="inline-flex items-center gap-2"><Icon name="ph:pencil-duotone" size="1.2rem"/><NuxtLink to="/proposer-une-correction">Editer cette page</NuxtLink></li>
         </ul>
+        <div class="flex flex-wrap gap-1">
+          <span v-for="tag in article.tags" :key="tag" class="inline-flex items-center gap-1">
+            <Icon name="ph:tag-duotone" size="1.2rem"/>
+            <NuxtLink :to="`/tags/${tag}`">{{ tag }}</NuxtLink>
+          </span>
+        </div>
+        <div>
+          <h5 class="mb-3 font-semibold text-xl">Contenu</h5>
+          <ul class="space-y-2">
+            <li v-for="header in headers"><NuxtLink :to="'#' + header.id">- {{ header.innerText }}</NuxtLink></li>
+          </ul>
+        </div>
       </div>
     </div>
   </main>
