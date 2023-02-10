bcc07e38c0c570b75d110d8e502e4bfab11938b6
Loeiz Moynet <moynetloeiz@gmail.com>
2023-02-10 09:10:24 +0100

    Fixed Search menu on mobile

--- components/Search.vue
+++ components/Search.vue
@@ -6,9 +6,6 @@ import {useFuse, UseFuseOptions} from "@vueuse/integrations/useFuse";
 const { data:tutoriels } = await useAsyncData('tutoriels', () => queryContent('tutoriels').find())
 const { data:astuces } = await useAsyncData('astuces', () => queryContent('astuces').find())
 const { data:codex } = await useAsyncData('codex', () => queryContent('codex').find())
-const { data:all } = await useAsyncData('content', () => queryContent('').find())
-
-const numberOfArticles = computed(() => 'Rechercher parmis ' + all.value?.length + ' articles...')
 
 const tutorielsFetched = ref<TutorialItem[]>(tutoriels)
 const astucesFetched = ref<AstuceItem[]>(astuces)
@@ -94,98 +91,100 @@ watch(showSearch, (newShowSearch) => {
 
 <template>
   <div class="form-control">
-    <input @input="showModalIfNotEmpty" v-model="search" ref="searchValue" type="text" :placeholder="numberOfArticles" class="search-value input input-bordered"/>
+    <input @input="showModalIfNotEmpty" v-model="search" ref="searchValue" type="text" placeholder="Rechercher..." class="search-value input input-bordered"/>
   </div>
   <Teleport to="body">
-    <div v-if="showSearch" class="flex items-center justify-center fixed inset-0 overflow-y-scroll bg-neutral/70 backdrop-blur z-40">
-      <div ref="modal" class="flex flex-col w-2/3 my-20 z-50">
-        <div class="block form-control mb-12">
-          <input v-model="search" ref="searchBar" type="text" :placeholder="numberOfArticles" class="input input-bordered w-1/2"/>
-        </div>
-        <div class="flex flex-col gap-10">
-          <div>
-            <div class="inline-flex items-baseline gap-2">
-              <h5 class="font-semibold text-2xl mb-2">Tutoriels</h5>
-              <NuxtLink @click="hideModal" to="/tutoriels">Tout voir</NuxtLink>
-            </div>
-            <ul class="flex gap-10 overflow-x-auto">
-              <li v-if="tutorielsResults.length >= 1" v-for="result in tutorielsResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                  <h2 class="card-title">{{ result.item.displayName }}</h2>
-                  <p>{{ result.item.description }}</p>
-                  <div class="card-actions justify-end">
-                    <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
-                  </div>
-                </div>
-              </li>
-              <li v-else>
-                <div class="card w-96 bg-neutral text-neutral-content">
-                  <div class="card-body">
-                    <h2 class="card-title">Oops !</h2>
-                    <p>Aucune astuce ne correspond à votre demande !</p>
-                    <div class="card-actions">
-                      <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une astuce <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
+    <div v-if="showSearch" class="fixed inset-0 bg-neutral/70 backdrop-blur z-40">
+      <div class="flex justify-center w-full h-full overflow-y-scroll">
+        <div ref="modal" class="flex flex-col w-2/3 my-20 z-50">
+          <div class="block form-control mb-12">
+            <input v-model="search" ref="searchBar" type="text" placeholder="Rechercher..." class="input input-bordered w-1/2"/>
           </div>
-          <div>
-            <div class="inline-flex items-baseline gap-2">
-              <h5 class="font-semibold text-2xl mb-2">Astuces</h5>
-              <NuxtLink @click="hideModal" to="/astuces">Tout voir</NuxtLink>
-            </div>
-            <ul class="flex gap-10 overflow-x-auto">
-              <li v-if="astucesResults.length >= 1" v-for="result in astucesResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                  <h2 class="card-title">{{ result.item.displayName }}</h2>
-                  <p>{{ result.item.description }}</p>
-                  <div class="card-actions justify-end">
-                    <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
-                  </div>
-                </div>
-              </li>
-              <li v-else>
-                <div class="card w-96 bg-neutral text-neutral-content">
+          <div class="flex flex-col gap-10">
+            <div>
+              <div class="inline-flex items-baseline gap-2">
+                <h5 class="font-semibold text-2xl mb-2">Tutoriels</h5>
+                <NuxtLink @click="hideModal" to="/tutoriels">Tout voir</NuxtLink>
+              </div>
+              <ul class="flex gap-10 overflow-x-auto">
+                <li v-if="tutorielsResults.length >= 1" v-for="result in tutorielsResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
                   <div class="card-body">
-                    <h2 class="card-title">Oops !</h2>
-                    <p>Aucune astuce ne correspond à votre demande !</p>
-                    <div class="card-actions">
-                      <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une astuce <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
+                    <h2 class="card-title">{{ result.item.displayName }}</h2>
+                    <p>{{ result.item.description }}</p>
+                    <div class="card-actions justify-end">
+                      <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
                     </div>
                   </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <div class="inline-flex items-baseline gap-2">
-              <h5 class="font-semibold text-2xl mb-2">Codex</h5>
-              <NuxtLink @click="hideModal" to="/codex">Tout voir</NuxtLink>
-            </div>
-            <ul class="flex gap-10 overflow-x-auto">
-              <li v-if="codexResults.length >= 1" v-for="result in codexResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                  <h2 class="card-title">{{ result.item.displayName }}</h2>
-                  <p>{{ result.item.description }}</p>
-                  <div class="card-actions justify-end">
-                    <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
-                  </div>
-                </div>
-              </li>
-              <li v-else>
-                <div class="card w-96 bg-neutral text-neutral-content">
-                  <div class="card-body">
-                    <h2 class="card-title">Oops !</h2>
-                    <p>Aucune entrée du Codex ne correspond à votre demande !</p>
-                    <div class="card-actions">
-                      <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une entrée de Codex <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
+                </li>
+                <li v-else>
+                  <div class="card w-96 bg-neutral text-neutral-content">
+                    <div class="card-body">
+                      <h2 class="card-title">Oops !</h2>
+                      <p>Aucune astuce ne correspond à votre demande !</p>
+                      <div class="card-actions">
+                        <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une astuce <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            </ul>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <div class="inline-flex items-baseline gap-2">
+                <h5 class="font-semibold text-2xl mb-2">Astuces</h5>
+                <NuxtLink @click="hideModal" to="/astuces">Tout voir</NuxtLink>
+              </div>
+              <ul class="flex gap-10 overflow-x-auto">
+                <li v-if="astucesResults.length >= 1" v-for="result in astucesResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
+                  <div class="card-body">
+                    <h2 class="card-title">{{ result.item.displayName }}</h2>
+                    <p>{{ result.item.description }}</p>
+                    <div class="card-actions justify-end">
+                      <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
+                    </div>
+                  </div>
+                </li>
+                <li v-else>
+                  <div class="card w-96 bg-neutral text-neutral-content">
+                    <div class="card-body">
+                      <h2 class="card-title">Oops !</h2>
+                      <p>Aucune astuce ne correspond à votre demande !</p>
+                      <div class="card-actions">
+                        <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une astuce <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <div class="inline-flex items-baseline gap-2">
+                <h5 class="font-semibold text-2xl mb-2">Codex</h5>
+                <NuxtLink @click="hideModal" to="/codex">Tout voir</NuxtLink>
+              </div>
+              <ul class="flex gap-10 overflow-x-auto">
+                <li v-if="codexResults.length >= 1" v-for="result in codexResults" class="card shrink-0 w-96 bg-base-100 shadow-xl">
+                  <div class="card-body">
+                    <h2 class="card-title">{{ result.item.displayName }}</h2>
+                    <p>{{ result.item.description }}</p>
+                    <div class="card-actions justify-end">
+                      <NuxtLink @click="hideModal" :to="result.item._path" class="btn btn-primary">Découvrir</NuxtLink>
+                    </div>
+                  </div>
+                </li>
+                <li v-else>
+                  <div class="card w-96 bg-neutral text-neutral-content">
+                    <div class="card-body">
+                      <h2 class="card-title">Oops !</h2>
+                      <p>Aucune entrée du Codex ne correspond à votre demande !</p>
+                      <div class="card-actions">
+                        <NuxtLink @click="hideModal" to="/tutoriels" class="btn btn-primary inline-flex gap-2" disabled>Proposer une entrée de Codex <span class="badge badge-ghost normal-case">Bientôt disponible</span></NuxtLink>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
